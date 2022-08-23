99b0f9900110d844a643cd9fbe42b672ddada925
Han-nahh <hannatesfaye11@gmail.com>
2022-08-23 23:32:40 +0300

    python regex

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/h?bt{1,}n/).join
+# Repetition Token #2
+puts ARGV[0].scan(/hbt+n/).join
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{0,}n/).join
+# Repetition Token #3
+puts ARGV[0].scan(/hbt*n/).join
