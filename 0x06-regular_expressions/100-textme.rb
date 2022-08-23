11edd60d7d73b6d9a83024a76f15e7320352c0cd
Han-nahh <hannatesfaye11@gmail.com>
2022-08-23 23:46:42 +0300

    python regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-# Textme
-puts ARGV[0]
-       .scan(/(?<=from:|to:|flags:).*?(?=\])/)
-       .join(',')
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
