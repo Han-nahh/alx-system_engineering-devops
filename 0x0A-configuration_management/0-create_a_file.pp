43151c989788be5ae3cf8b683bf042404e258314
Han-nahh <100560594+Han-nahh@users.noreply.github.com>
2022-11-21 17:15:28 +0300

    Add files via upload

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
+# Create a file
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
